b4f2a4fe6158ecb7e953ad953fa4cca7ef4227e3
Jessica Frazelle <jess@docker.com>
2014-10-09 10:02:35 -0700

    sentry changes

--- sentry/sentry.conf.py
+++ sentry/sentry.conf.py
@@ -27,7 +27,7 @@ SENTRY_PUBLIC = False
 
 # You should configure the absolute URI to Sentry. It will attempt to guess it if you don't
 # but proxies may interfere with this.
-# SENTRY_URL_PREFIX = 'http://sentry.example.com'  # No trailing slash!
+SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX', 'http://sentry.example.com')  # No trailing slash!
 
 SENTRY_WEB_HOST = '0.0.0.0'
 SENTRY_WEB_PORT = 9000
