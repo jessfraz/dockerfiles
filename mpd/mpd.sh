29b458b672db111fc30ae0f4e750c8cb82251c52
Jessica Frazelle <princess@docker.com>
2015-06-06 22:08:01 -0700

    add music player daemon
    Signed-off-by: Jessica Frazelle <princess@docker.com>

---
+++ mpd/mpd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+mkdir -p /var/lib/mpd/{playlists,music} \
+	&& touch /var/lib/mpd/{state,tag_cache} \
+	&& chmod 0777 -R /var/lib/mpd \
+	&& chown -R mpd /var/lib/mpd
+
+exec mpd --no-daemon --stdout --verbose /etc/mpd.conf "$@"
