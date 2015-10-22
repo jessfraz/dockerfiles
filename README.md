cb791cab290013bf6d9b51389c4b7cd443df18c4
Jessica Frazelle <acidburn@docker.com>
2015-10-22 02:13:39 -0700

    add circleci
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## dockerfiles
 
+[![Circle CI](https://circleci.com/gh/jfrazelle/dockerfiles.svg?style=svg)](https://circleci.com/gh/jfrazelle/dockerfiles)
+
 This is a repo to hold various Dockerfiles for images I create.
 
-I try to make sure each has a command at the top for running it, 
+I try to make sure each has a command at the top for running it,
 if a file you are looking at does not have a command, please
 pull request it!
 
---
+++ test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+# this is kind of an expensive check, so let's not do this twice if we
+# are running more than one validate bundlescript
+VALIDATE_REPO='https://github.com/jfrazelle/dockerfiles.git'
+VALIDATE_BRANCH='master'
+
+VALIDATE_HEAD="$(git rev-parse --verify HEAD)"
+
+git fetch -q "$VALIDATE_REPO" "refs/heads/$VALIDATE_BRANCH"
+VALIDATE_UPSTREAM="$(git rev-parse --verify FETCH_HEAD)"
+
+VALIDATE_COMMIT_DIFF="$VALIDATE_UPSTREAM...$VALIDATE_HEAD"
+
+validate_diff() {
+	if [ "$VALIDATE_UPSTREAM" != "$VALIDATE_HEAD" ]; then
+		git diff "$VALIDATE_COMMIT_DIFF" "$@"
+	else
+		git diff HEAD~ "$@"
+	fi
+}
+
+# get the dockerfiles changed
+IFS=$'\n'
+files=( $(validate_diff --name-only -- '*Dockerfile') )
+unset IFS
+
+# build the changed dockerfiles
+for f in "${files[@]}"; do
+	image=${f%Dockerfile}
+	base=${image%%\/*}
+	suite=${image##*\/}
+	build_dir=$(dirname $f)
+
+	if [[ -z "$suite" ]]; then
+		suite=latest
+	fi
+
+	(
+	set -x
+	docker build -t ${base}:${suite} ${build_dir}
+	)
+
+	echo "                       ---                                   "
+	echo "Successfully built ${base}:${suite} with context ${build_dir}"
+	echo "                       ---                                   "
+done
