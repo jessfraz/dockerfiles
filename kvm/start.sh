c91afc01d0c16fed67d82c18d56fdb8f1b6aab97
Jess Frazelle <jess@mesosphere.com>
2016-03-26 14:41:58 -0700

    virt-viewer
    Signed-off-by: Jess Frazelle <jess@mesosphere.com>

--- kvm/start.sh
+++ kvm/start.sh
@@ -8,10 +8,10 @@ chown root:kvm /dev/kvm
 
 # create the bridge for networking
 ip link add name virt0 type bridge
-ip link set virt0 up
+ip link set dev virt0 up
 bridge link
-ip addr add dev virt0 192.168.66.66/24
-iptables -t nat -A POSTROUTING -s 192.168.66.66/24 -j MASQUERADE
+ip addr add dev virt0 172.20.0.1/16
+iptables -t nat -A POSTROUTING -s 172.20.0.1/16 -j MASQUERADE
 
 # start the virtlogd daemon
 exec virtlogd --daemon &
