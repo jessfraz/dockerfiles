a85814d20e48068fb8f329a51791eff2b6a514e5
Jess Frazelle <jess@mesosphere.com>
2016-04-06 03:32:01 -0700

    updates and cleanups
    Signed-off-by: Jess Frazelle <jess@mesosphere.com>

---
+++ gitiles/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+ROOT="/gitiles"
+PROPERTIES=
+
+if [ "x$1" != "x" ]; then
+  PROPERTIES="-Dcom.google.gitiles.configPath=$1"
+fi
+PROPERTIES="$PROPERTIES -Dcom.google.gitiles.sourcePath=$ROOT"
+
+exec java $PROPERTIES -jar "$ROOT/buck-out/gen/gitiles-dev/dev.jar"
---
+++ gitserver/start.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+set -e
+
+[ "$DEBUG" == 'true' ] && set -x
+
+DAEMON=sshd
+HOSTKEY=/etc/ssh/ssh_host_ed25519_key
+
+# create the host key if not already created
+if [ ! -f "${HOSTKEY}" ]; then
+	ssh-keygen -A
+fi
+
+[ "$PUBKEY" ] && echo "$PUBKEY" > ${HOME}/.ssh/authorized_keys
+
+# Fix permissions, if writable
+if [ -w ${HOME}/.ssh ]; then
+    chown git:git ${HOME}/.ssh && chmod 700 ${HOME}/.ssh/
+fi
+if [ -w ${HOME}/.ssh/authorized_keys ]; then
+    chown git:git ${HOME}/.ssh/authorized_keys
+    chmod 600 ${HOME}/.ssh/authorized_keys
+fi
+
+# Warn if no config
+if [ ! -e ${HOME}/.ssh/authorized_keys ]; then
+  echo "WARNING: No SSH authorized_keys found for git"
+fi
+
+# set the default shell
+mkdir -p $HOME/git-shell-commands
+cat >$HOME/git-shell-commands/no-interactive-login <<\EOF
+#!/bin/sh
+printf '%s\n' "Hi $USER! You've successfully authenticated, but I do not"
+printf '%s\n' "provide interactive shell access."
+exit 128
+EOF
+chmod +x $HOME/git-shell-commands/no-interactive-login
+
+stop() {
+    echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
+    # Get PID
+    pid=$(cat /var/run/$DAEMON/$DAEMON.pid)
+    # Set TERM
+    kill -SIGTERM "${pid}"
+    # Wait for exit
+    wait "${pid}"
+    # All done.
+    echo "Done."
+}
+
+echo "Running $@"
+if [ "$(basename $1)" == "$DAEMON" ]; then
+    trap stop SIGINT SIGTERM
+    $@ &
+    pid="$!"
+    mkdir -p /var/run/$DAEMON && echo "${pid}" > /var/run/$DAEMON/$DAEMON.pid
+    wait "${pid}" && exit $?
+else
+    exec "$@"
+fi
