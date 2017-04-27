d2bb8fbcf30501b7b0fc971085137f521efb6435
Jess Frazelle <acidburn@google.com>
2017-04-27 17:06:24 -0400

    latest versions
    Signed-off-by: Jess Frazelle <acidburn@google.com>

--- vscode/start.sh
+++ vscode/start.sh
@@ -2,10 +2,5 @@
 set -e
 set -o pipefail
 
-VSCODE_COMMAND=/usr/bin/code
-if [[ ! -f "${VSCODE_COMMAND}" ]]; then
-	>&2 echo "${VSCODE_COMMAND} does not exist"
-	exit 1
-fi
-su user -c "${VSCODE_COMMAND}"
+su user -c /usr/share/code/bin/code
 sleep infinity
