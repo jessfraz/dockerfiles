31f2da120e8b3ad06a49fc11b0745aa77e37e9ac
Jess Frazelle <acidburn@google.com>
2017-06-09 14:00:18 -0400

    updates and cleanup
    Signed-off-by: Jess Frazelle <acidburn@google.com>

--- sublime-text-3/run.sh
+++ sublime-text-3/run.sh
@@ -2,11 +2,13 @@
 set -e
 set -o pipefail
 
+COMMAND=/opt/sublime_text/sublime_text
+
 if [ -z ${NEWUSER+x} ]; then
 	echo 'WARN: No user was defined, defaulting to root.'
 	echo 'WARN: Sublime will save files as root:root.'
 	echo '      To prevent this, start the container with -e NEWUSER=$USER'
-	/usr/src/sublime_text/sublime_text -w
+	exec $COMMAND -w
 else
 	# The root user already exists, so we only need to do something if
 	# a user has been specified.
@@ -14,6 +16,6 @@ else
 	# If you'd like to have Sublime Text add your development folder
 	# to the current project (i.e. in the sidebar at start), append
 	# "-a /home/$NEWUSER/Documents" (without quotes) into the su -c command below.
-	# Example: su $NEWUSER -c "/usr/src/sublime_text/sublime_text -w -a /home/$NEWUSER/Documents"
-	su $NEWUSER -c "/usr/src/sublime_text/sublime_text -w"
+	# Example: su $NEWUSER -c "$COMMAND -w -a /home/$NEWUSER/Documents"
+	su $NEWUSER -c "$COMMAND -w"
 fi
