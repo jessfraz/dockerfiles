dd0d53927eb8aba222ac9cc98032bf9f583543bf
Jess Frazelle <acidburn@microsoft.com>
2017-11-27 15:20:56 -0500

    ykman
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ ykman/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+init(){
+	local pcscd_running=$(ps -aux | grep [p]cscd)
+	if [ -z "$pcscd_running" ]; then
+		echo "starting pcscd in backgroud"
+		pcscd --debug --apdu
+		pcscd --hotplug
+	else
+		echo "pcscd is running in already: ${pcscd_running}"
+	fi
+}
+
+init
+
+"$@"
