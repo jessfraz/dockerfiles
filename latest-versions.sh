4c453b48dc15095040a0c5518b47c7a4c03c80a7
Jess Frazelle <acidburn@microsoft.com>
2018-05-28 22:53:22 -0400

    fix latest versions
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- latest-versions.sh
+++ latest-versions.sh
@@ -45,6 +45,8 @@ get_latest() {
 		dir="zookeeper/3.5"
 	elif [[ "$dir" == "oauth2_proxy" ]]; then
 		dir="oauth2-proxy"
+	elif [[ "$dir" == "wireguard" ]]; then
+		dir="wireguard/install"
 	fi
 
 	# Change to upper case for grep
