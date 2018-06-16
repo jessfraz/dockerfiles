1bd870933e253ac828560fc96751ca82e292f258
Aaron Ten Clay <aarontc@aarontc.com>
2018-06-16 01:48:03 -0700

    Add  configuration for tor-relay

--- tor-relay/run.sh
+++ tor-relay/run.sh
@@ -9,6 +9,7 @@ for relaytype in bridge middle exit; do
 	sed -i 's/${CONTACT_EMAIL}/'"$CONTACT_EMAIL"'/g' "/etc/tor/torrc.$relaytype"
 	sed -i 's/${RELAY_BANDWIDTH_RATE}/'"$RELAY_BANDWIDTH_RATE"'/g' "/etc/tor/torrc.$relaytype"
 	sed -i 's/${RELAY_BANDWIDTH_BURST}/'"$RELAY_BANDWIDTH_BURST"'/g' "/etc/tor/torrc.$relaytype"
+	sed -i 's/${RELAY_PORT}/'"$RELAY_PORT"'/g' "/etc/tor/torrc.$relaytype"
 done
 
 exec tor -f /etc/tor/torrc.${RELAY_TYPE}
