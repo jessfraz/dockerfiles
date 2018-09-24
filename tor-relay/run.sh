55513d40f65ad48f17eb3a45232285581988cd26
Jess Frazelle <acidburn@microsoft.com>
2018-09-24 18:42:35 -0400

    update tor-relay
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- tor-relay/run.sh
+++ tor-relay/run.sh
@@ -3,13 +3,15 @@ set -e
 set -o pipefail
 
 for relaytype in bridge middle exit; do
-	sed -i 's/${RELAY_NICKNAME}/'"$RELAY_NICKNAME"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${CONTACT_GPG_FINGERPRINT}/'"$CONTACT_GPG_FINGERPRINT"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${CONTACT_NAME}/'"$CONTACT_NAME"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${CONTACT_EMAIL}/'"$CONTACT_EMAIL"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${RELAY_BANDWIDTH_RATE}/'"$RELAY_BANDWIDTH_RATE"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${RELAY_BANDWIDTH_BURST}/'"$RELAY_BANDWIDTH_BURST"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${RELAY_PORT}/'"$RELAY_PORT"'/g' "/etc/tor/torrc.$relaytype"
+	file="/etc/tor/torrc.${relaytype}"
+
+	sed -i "s/RELAY_NICKNAME/${RELAY_NICKNAME}/g" "$file"
+	sed -i "s/CONTACT_GPG_FINGERPRINT/${CONTACT_GPG_FINGERPRINT}/g" "$file"
+	sed -i "s/CONTACT_NAME/${CONTACT_NAME}/g" "$file"
+	sed -i "s/CONTACT_EMAIL/${CONTACT_EMAIL}/g" "$file"
+	sed -i "s/RELAY_BANDWIDTH_RATE/${RELAY_BANDWIDTH_RATE}/g" "$file"
+	sed -i "s/RELAY_BANDWIDTH_BURST/${RELAY_BANDWIDTH_BURST}/g" "$file"
+	sed -i "s/RELAY_PORT/${RELAY_PORT}/g" "$file"
 done
 
 exec tor -f "/etc/tor/torrc.${RELAY_TYPE}"
