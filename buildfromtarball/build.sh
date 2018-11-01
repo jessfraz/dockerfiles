8b4b42e3a3173cf0d139054ad0a0049730c72242
Jess Frazelle <acidburn@microsoft.com>
2018-11-01 19:14:41 -0400

    fix
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- buildfromtarball/build.sh
+++ buildfromtarball/build.sh
@@ -8,14 +8,14 @@ image=$2
 
 volume=/build
 
-mkdir -p "$build"
+mkdir -p "$volume"
 
-curl --fail --silent --show-error --location "$tarball" | tar xvz --strip=1 -C %s 2>/dev/null) || (echo ""; printf "ERROR: %%s\n" "Could not prepare an image." "Please verify that $tarball is still available and is publicly accessible."; exit 1)
+(curl --fail --silent --show-error --location "$tarball" | tar xvz --strip=1 -C "$volume" 2>/dev/null) || (echo ""; echo "ERROR: Could not prepare an image.";echo "Please verify that $tarball is still available and is publicly accessible."; exit 1;)
 
 # Try to pull the image.
 docker pull "$image" || true
 
-cd "$build"
+cd "$volume"
 
 docker build --cache-from "$image" -t "$image" .
 
